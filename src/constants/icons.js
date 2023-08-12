5cdea37c3b386880241cdd2667171962efa7b666
FarruxDEV <farruxbeknozimov@gmail.com>
2023-08-12 20:15:03 +0500

    all changes

---
+++ src/constants/icons.js
@@ -0,0 +1,19 @@
+export const icons = {
+  ACCESSIBILITY: [
+    'lab la-accessible-icon',
+    'las la-american-sign-language-interpreting',
+    'las la-assistive-listening-systems',
+    'las la-audio-description',
+    'las la-blind',
+    'las la-braille',
+    'las la-closed-captioning',
+    'las la-deaf',
+    'las la-low-vision',
+    'las la-phone-volume',
+    'las la-question-circle',
+    'las la-sign-language',
+    'las la-tty',
+    'las la-universal-acces',
+    'las la-wheelchair'
+  ]
+}
--- src/views/Icons.vue
+++ src/views/Icons.vue
@@ -1,20 +1,16 @@
 <script setup>
-try {
-  const icons = await fetch(
-    'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css'
-  )
-  console.log(icons.text())
-} catch (error) {
-  console.log(error)
-}
+import { icons } from '@/constants/icons'
 </script>
 
 <template>
-  <div class="bg-white p-5 shadow rounded">
-    {{ icons }}
-    {{ icons.text() }}
-    <h3 class="text-gray-800 mb-3">Icons</h3>
-    <h3 class="text-gray-600 font-[400]">ACCESSIBILITY</h3>
+  <div class="h-screen">
+    <div class="bg-white p-5 shadow rounded">
+      <h3 class="text-gray-800 mb-3">Icons</h3>
+
+      <div v-for="(el, i) in icons" class="">
+        <h3 class="text-gray-600">{{ i }}</h3>
+      </div>
+    </div>
   </div>
 </template>
 
